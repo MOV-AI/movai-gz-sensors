8fc95e7cc3f6973f49884be844565854e2d671e0
Mariana Alves <mariana.alves@mov.ai>
2023-12-11 12:36:52 +0000

    Add deploy workflow

---
+++ .github/ci/after_make_test.sh
@@ -0,0 +1,34 @@
+# Package 
+mkdir artifacts
+cd artifacts && make package
+cd ..
+
+# Deploy to nexus
+NEXUS_REPO="ppa-public"
+NEXUS_ENDPOINT="artifacts.aws.cloud.mov.ai"
+
+for file in artifacts/*.deb
+do
+    RETURN_CODE=$(curl -u "$nexus_publisher_user:$nexus_publisher_password" \
+    -H "Content-Type: multipart/form-data" \
+    --data-binary "@$file" \
+    -w '%{http_code}' \
+    "https://$NEXUS_ENDPOINT/repository/$NEXUS_REPO/")
+
+    #retry
+    if [[ ! "$RETURN_CODE" =~ ^(200|201|202)$ ]]; then
+    echo "Failed upload with $RETURN_CODE. Retrying"
+
+    RETURN_CODE=$(curl -u "$nexus_publisher_user:$nexus_publisher_password" \
+        -H "Content-Type: multipart/form-data" \
+        --data-binary "@$file" \
+        -w '%{http_code}' \
+        "https://$NEXUS_ENDPOINT/repository/$NEXUS_REPO/")
+    fi
+
+    if [[ ! "$RETURN_CODE" =~ ^(200|201|202)$ ]]; then
+    echo "Failed upload with $RETURN_CODE. Exiting"
+    exit 1
+    fi
+
+done
