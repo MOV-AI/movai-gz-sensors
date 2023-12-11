ba8868aed5addbb885ad5497083fc841517ed09a
Mariana Alves <mariana.alves@mov.ai>
2023-12-11 16:36:04 +0000

    Change package name and separate deployment to task

--- .github/ci/after_make_test.sh
+++ .github/ci/after_make_test.sh
@@ -1,33 +1,3 @@
 # Package 
 make package
-mkdir artifacts && mv *.deb artifacts/
-
-# Deploy to nexus
-NEXUS_REPO="ppa-public"
-NEXUS_ENDPOINT="artifacts.aws.cloud.mov.ai"
-
-for file in artifacts/*.deb
-do
-    RETURN_CODE=$(curl -u "$nexus_publisher_user:$nexus_publisher_password" \
-    -H "Content-Type: multipart/form-data" \
-    --data-binary "@$file" \
-    -w '%{http_code}' \
-    "https://$NEXUS_ENDPOINT/repository/$NEXUS_REPO/")
-
-    #retry
-    if [[ ! "$RETURN_CODE" =~ ^(200|201|202)$ ]]; then
-    echo "Failed upload with $RETURN_CODE. Retrying"
-
-    RETURN_CODE=$(curl -u "$nexus_publisher_user:$nexus_publisher_password" \
-        -H "Content-Type: multipart/form-data" \
-        --data-binary "@$file" \
-        -w '%{http_code}' \
-        "https://$NEXUS_ENDPOINT/repository/$NEXUS_REPO/")
-    fi
-
-    if [[ ! "$RETURN_CODE" =~ ^(200|201|202)$ ]]; then
-    echo "Failed upload with $RETURN_CODE. Exiting"
-    exit 1
-    fi
-
-done
+mkdir ../artifacts && mv *.deb ../artifacts/
